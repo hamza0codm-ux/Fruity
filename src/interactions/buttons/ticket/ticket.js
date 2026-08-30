c351923cf351feaf8ba0fab7e6a0f0764e64ac0e
hamza0codm-ux <hamza0codm@gmail.com>
2026-08-30 21:00:00 +0200

    Update ticket.js

--- src/interactions/buttons/ticket/ticket.js
+++ src/interactions/buttons/ticket/ticket.js
@@ -8,8 +8,24 @@ import createTicketHandler, {
   deleteTicketHandler,
 } from '../../../handlers/ticketButtons.js';
 
+import {
+  normalTicketCreateButton,
+} from '../../../tickets/normalTickets.js';
+
+import {
+  merchTicketCreateButton,
+} from '../../../tickets/merchTickets.js';
+
 export default [
   createTicketHandler,
+
+  // Normal Tickets panel
+  normalTicketCreateButton,
+
+  // Merch Tickets panel
+  merchTicketCreateButton,
+
+  // Existing ticket controls
   closeTicketHandler,
   claimTicketHandler,
   priorityTicketHandler,
