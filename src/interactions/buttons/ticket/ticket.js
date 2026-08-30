aab60d081c8a9941646d2e438ea3aa194f5f6173
hamza0codm-ux <hamza0codm@gmail.com>
2026-08-30 23:55:49 +0200

    Update ticket.js

--- src/interactions/buttons/ticket/ticket.js
+++ src/interactions/buttons/ticket/ticket.js
@@ -6,51 +6,8 @@ import {
     merchTicketCreateButton,
 } from '../../../tickets/merchTickets.js';
 
-import {
-    closeTicketHandler,
-    claimTicketHandler,
-    priorityMenuHandler,
-    priorityTicketHandler,
-    pinTicketHandler,
-    unclaimTicketHandler,
-    reopenTicketHandler,
-    deleteTicketHandler,
-} from '../../../handlers/ticketButtons.js';
-
 
 export default [
-
-    // =========================================================
-    // NORMAL TICKET CREATION
-    // =========================================================
-
     normalTicketCreateButton,
-
-
-    // =========================================================
-    // MERCH TICKET CREATION
-    // =========================================================
-
     merchTicketCreateButton,
-
-
-    // =========================================================
-    // TICKET CONTROLS
-    // =========================================================
-
-    closeTicketHandler,
-
-    claimTicketHandler,
-
-    priorityMenuHandler,
-
-    priorityTicketHandler,
-
-    pinTicketHandler,
-
-    unclaimTicketHandler,
-
-    reopenTicketHandler,
-
-    deleteTicketHandler,
 ];
