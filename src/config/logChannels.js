78bd83f102870d090aff5368f8d527e6afbc09b4
hamza0codm-ux <hamza0codm@gmail.com>
2026-08-31 22:27:36 +0200

    Create logChannels.js

---
+++ src/config/logChannels.js
@@ -0,0 +1,15 @@
+export const LOG_CHANNELS = {
+    moderation: '1542845968012021760',
+    messages: '1542858198233653348',
+    roleChannel: '1542860408082276392',
+    members: '1542858527121604658',
+    botIntegrations: '1544080337649279137',
+    security: '1541557303453683792',
+};
+
+export const LOG_COLORS = {
+    green: 0x57F287,
+    yellow: 0xFEE75C,
+    red: 0xED4245,
+    orange: 0xF5A623,
+};
